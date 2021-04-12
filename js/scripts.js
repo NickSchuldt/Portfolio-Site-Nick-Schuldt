37c189c8e3813e19e3b29a5883c26406d5f41cc8
NickSchuldt <nickschuldt1@gmail.com>
2021-04-11 23:45:38 -0400

    2.3 Js menu update
    Added hamburger menu
    Added jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-console.log("Hey! Welcome to My Porfolio Site")
+<console.log("Hey! Welcome to My Porfolio Site")
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += 'responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
