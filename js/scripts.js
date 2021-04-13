91e0e60d41f174ad6435d1f9beb3a75b520d88db
NickSchuldt <nickschuldt1@gmail.com>
2021-04-13 00:19:34 -0400

    2.3 CSS Updates
    Fixed hover issue.
    Menu drops down properly now.
    Added menu to Experts page
    Changed Margins for experts page

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ console.log("Hey! Welcome to My Porfolio Site");
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
-    x.className += 'responsive';
+    x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
   }
